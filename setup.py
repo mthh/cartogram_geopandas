580597e44a4e373c70fbd62c9153f64513c2acc4
mthh <mthh@users.noreply.github.com>
2015-09-23 15:34:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 """
 cartogram_geopandas
 """
+
 from distutils.core import setup
+from Cython.Build import cythonize
 
 setup(
     name='cartogram_geopandas',
-    version='0.0.0b',
-    description='Python module for easy cartogram creation on GeoDataFrame',
+    version='0.0.0c',
+    description='Fast and convenient cartogram creation on a GeoDataFrame',
     author='mthh',
     py_modules=['cartogram_geopandas'],
+    ext_modules=cythonize("cycartogram.pyx"),
     license='GPL v2',
     )
